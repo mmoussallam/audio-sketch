6d28b7d4555da3cd16d435119d3c0e5febdfaa42
Manuel Moussallam <manuel.moussallam@gmail.com>
2013-01-31 18:55:45 +0100

    added 2 sketches, stft peaks and MP on union mdct

--- src/classes/sketch.py
+++ src/classes/sketch.py
@@ -3,7 +3,9 @@ Created on Jan 30, 2013
 
 @author: manu
 '''
-
+import numpy as np
+from PyMP import Signal
+import matplotlib.pyplot as plt
 class AudioSketch(object):
     ''' This class should be used as an abstract one, specify the
     audio sketches interface. 
@@ -33,9 +35,241 @@ class AudioSketch(object):
                 e.g. it can implement a thresholding or a peak-picking method
     
     `represent`  optionnal: a nice representation of the sketch
+    
+    `fgpt`        build a fingerprint from the set of parameters
     '''
 
 
-def cochleogram(data, params):
-    ''' Build a cochleogram object from a set of parameters for
-    given data, in the form of an auditory spectrum ''' 
+    params = {}             # dictionary of parameters
+    orig_signal = None      # original Signal object
+    rec_signal = None       # reconstructed Signal object
+    rep = None              # representation handle
+    sp_rep = None           # sparsified representation handle
+    
+    
+    def __repr__(self):
+        strret = ''' 
+%s  %s
+Params: %s ''' % (self.__class__.__name__, str(self.orig_signal),str(self.params))
+        return  strret
+    
+    def synthesize(self, synthparams):    
+        raise NotImplementedError("NOT IMPLEMENTED: ABSTRACT CLASS METHOD CALLED")
+    
+    def represent(self):
+        raise NotImplementedError("NOT IMPLEMENTED: ABSTRACT CLASS METHOD CALLED")
+
+    def fgpt(self):
+        raise NotImplementedError("NOT IMPLEMENTED: ABSTRACT CLASS METHOD CALLED")
+    
+    def recompute(self):
+        raise NotImplementedError("NOT IMPLEMENTED: ABSTRACT CLASS METHOD CALLED")
+    
+    def sparsify(self, sparsity):
+        raise NotImplementedError("NOT IMPLEMENTED: ABSTRACT CLASS METHOD CALLED")
+        
+        
+class STFTPeaksSketch(AudioSketch):
+    ''' Sketch based on a single STFT with peak-picking as a 
+    sparsifying method '''
+    
+    # baseline parameters: default rectangle is 10 frames by 10 bins large
+    params = {'scale':1024,
+              'step':512,
+              'f_width':10,
+              't_width':10}
+            
+    # TODO this is same as superclass
+    def __init__(self, original_sig=None, **kwargs):        
+        # add all the parameters that you want
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+        
+        
+        if original_sig is not None:
+            self.orig_signal = original_sig
+            self.recompute() 
+
+    def recompute(self, signal = None):
+
+        if signal is not None:
+            if isinstance(signal, str):
+                # TODO allow for stereo signals
+                signal = Signal(signal, normalize=True, mono=True)
+            self.orig_signal = signal
+        
+        if self.orig_signal is None:
+            raise ValueError("No original Sound has been given")
+
+        import stft
+        self.rep = stft.stft(self.orig_signal.data,
+                             self.params['scale'],
+                             self.params['step'])
+
+    def represent(self):
+        import matplotlib.pyplot as plt
+        plt.figure()
+        for chanIdx in range(self.rep.shape[0]):
+            plt.subplot(self.rep.shape[0],1,chanIdx+1)
+            plt.imshow(10*np.log10(np.abs(self.rep[chanIdx,:,:])),
+               aspect='auto',
+               interpolation='nearest',
+               origin='lower')
+    
+    def sparsify(self, sparsity, **kwargs):
+        ''' sparsity is here achieved through Peak-Picking in the 
+        STFT: naive version: square TF regions'''
+        if self.rep is None:
+            raise ValueError("representation hasn't been computed yet..")
+        
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+        else:
+            # otherwise the sparsity argument take over and we divide in 
+            # the desired number of regions (preserving the bin/frame ratio)
+#            print self.rep.shape[1:]
+            self.params['f_width'] = int(self.rep.shape[1]/np.sqrt(sparsity))
+            self.params['t_width'] = int(self.rep.shape[2]/np.sqrt(sparsity))
+#            print self.params['f_width'], self.params['t_width']
+            
+        self.sp_rep = np.zeros_like(self.rep)
+        
+        # naive implementation: cut in non-overlapping zone and get the max
+        (n_bins, n_frames) = self.rep.shape[1:]
+        
+        
+        (f,t) = (self.params['f_width'], self.params['t_width'])
+        
+        
+        
+        for x_ind in range(0, (n_frames/t)*t, t):
+            for y_ind in range(0, (n_bins/f)*f, f):
+#                print y_ind, x_ind
+                rect_data = self.rep[0,y_ind:y_ind+f,x_ind:x_ind+t]
+                
+                if len(rect_data)>0 and (np.sum(rect_data**2) > 0):
+                    f_index, t_index = divmod(np.abs(rect_data).argmax(), t)
+#                    print f_index, t_index
+#                    print y_ind, x_ind, rect_data
+                    # add the peak to the sparse rep
+                    self.sp_rep[0, y_ind + f_index, x_ind + t_index] = rect_data[f_index, t_index]
+        
+        self.nnz = np.count_nonzero(self.sp_rep)
+        print "Sparse rep of %d element computed" % self.nnz
+        
+        
+    def represent_sparse(self):
+        if self.sp_rep is None:
+            raise ValueError("no sparse rep constructed yet")
+        import matplotlib.pyplot as plt
+        plt.figure()
+        for chanIdx in range(self.sp_rep.shape[0]):
+            plt.subplot(self.sp_rep.shape[0],1,chanIdx+1)
+            plt.imshow(10*np.log10(np.abs(self.sp_rep[chanIdx,:,:])),
+               aspect='auto',
+               interpolation='nearest',
+               origin='lower')
+
+    def synthesize(self):
+        import stft
+        self.rec_signal = Signal(stft.istft(self.sp_rep,
+                                            self.params['step'],
+                                            self.orig_signal.length),
+                                 self.orig_signal.fs)
+        return self.rec_signal
+
+
+class XMDCTSparseSketch(AudioSketch):
+    ''' A sketching based on MP with a union of MDCT basis '''
+    
+    
+    # baseline parameters: default rectangle is 10 frames by 10 bins large
+    params = {'scales':[128,1024,8192],
+              'nature':'LODico',
+              'n_atoms':1000,
+              'SRR':30}
+
+    # TODO this is same as superclass
+    def __init__(self, original_sig=None, **kwargs):        
+        # add all the parameters that you want
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+                
+        if original_sig is not None:
+            self.orig_signal = original_sig
+            self.recompute() 
+            
+    def recompute(self, signal = None, **kwargs):
+        for key in kwargs:
+            self.params[key] = kwargs[key]
+
+        if signal is not None:
+            if isinstance(signal, str):
+                # TODO allow for stereo signals
+                signal = Signal(signal, normalize=True, mono=True)
+            self.orig_signal = signal
+        
+        if self.orig_signal is None:
+            raise ValueError("No original Sound has been given")
+
+        from PyMP.mdct import Dico
+        from PyMP import mp
+        mdct_dico = Dico(self.params['scales'])
+        self.rep = mp.mp(self.orig_signal,
+                         mdct_dico,
+                         self.params['SRR'],
+                         self.params['n_atoms'],
+                         debug=0)[0]
+        
+    def represent(self, fig=None):
+        if fig is None:
+            plt.figure()
+            
+        self.rep.plot_tf()
+        
+    def represent_sparse(self, fig=None):                
+        if fig is None:
+            fig = plt.figure()        
+        
+        if self.sp_rep is None:
+            return self.represent(fig)
+                
+        self.sp_rep.plot_tf()
+        
+    def sparsify(self, sparsity, **kwargs):
+        ''' here behaviour is this:
+        if sparsity > current number of atoms: pursue the decomposition
+        else return the desired number of atoms as sp_rep'''
+
+        if self.rep is None:
+            self.params['n_atoms'] = sparsity
+            self.recompute(**kwargs)
+            self.sp_rep = self.rep
+        
+        elif sparsity > self.rep.atom_number:
+            self.params['n_atoms'] = sparsity
+            # Sparsity asked in more than has been computed
+            from PyMP.mdct import Dico
+            from PyMP import mp
+            self.sp_rep = mp.mp_continue(self.rep,
+                                         self.orig_signal,
+                                         Dico(self.params['scales']),
+                                         self.params['SRR'],
+                                         self.params['n_atoms']-self.rep.atom_number,
+                                         pad=False)[0]
+        
+        else:
+            # thanks to getitem: it will construct a new approx object
+            # with the #sparsity biggest atoms
+            self.sp_rep = self.rep[:sparsity]
+        
+        self.nnz = self.sp_rep.atom_number        
+        print "Sparse rep of %d element computed" % self.nnz
+    
+    def synthesize(self):
+        if self.sp_rep is None:
+            return self.rep.recomposed_signal
+        else:
+            return self.sp_rep.recomposed_signal
+        
+        
--- src/tests/SketchTests.py
+++ src/tests/SketchTests.py
@@ -7,15 +7,70 @@ import unittest
 import sys
 sys.path.append('/home/manu/workspace/audio-sketch')
 sys.path.append('/home/manu/workspace/PyMP')
+sys.path.append('/home/manu/workspace/meeg_denoise')
 
-class GiantTest(unittest.TestCase):
+from classes import sketch
+import matplotlib.pyplot as plt
+plt.switch_backend('Agg')
+audio_test_file = '/sons/tests/Bach_prelude_4s.wav'
 
-    
+class SketchTest(unittest.TestCase):
 
-    def testName(self):
-        pass
 
+    def runTest(self):
+                
+        abstractSketch = sketch.AudioSketch()
+        print abstractSketch
+        self.assertRaises(NotImplementedError,abstractSketch.represent)
+        self.assertRaises(NotImplementedError,abstractSketch.recompute)
+        self.assertRaises(NotImplementedError,abstractSketch.sparsify, (0))
+        self.assertRaises(NotImplementedError,abstractSketch.synthesize, ({}))
+        
+#        kwargs = {'scale':512, 'step':256}
+#        stftpeaksketch = sketch.STFTPeaksSketch(**kwargs)
+#        
+#        kwargs = {'dico':[64,512,2048], 'n_atoms':100}
+#        xmdctmpsketch = sketch.XMDCTSparseSketch()
+        
+        sketches_to_test = [sketch.XMDCTSparseSketch(**{'scales':[64,512,2048], 'n_atoms':100}),
+                            sketch.STFTPeaksSketch(**{'scale':512, 'step':256}),                            
+                            ]
+        
+        # for all sketches, we performe the same testing
+        for sk in sketches_to_test:
+            print sk
+            self.assertRaises(ValueError, sk.recompute)
+            
+            print " compute full representation"
+            sk.recompute(audio_test_file)
+            
+            print " plot the computed full representation" 
+            sk.represent()
+            
+            print " Now sparsify with 1000 elements" 
+            sk.sparsify(1000)
+            
+            print " plot the sparsified representation"
+            sk.represent_sparse()
+            
+            print " and synthesize the sketch"
+            synth_sig = sk.synthesize()
+            
+            plt.figure()
+            plt.subplot(211)
+            plt.plot(sk.orig_signal.data)
+            plt.subplot(212)
+            plt.plot(synth_sig.data)
+            
+#            synth_sig.play()
+            synth_sig.write('Test_%s.wav'%sk.__class__.__name__)
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    
+    suite = unittest.TestSuite()
+
+    suite.addTest(SketchTest())
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    plt.show()
+    
