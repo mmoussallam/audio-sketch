da51f87b4bb710bd7a1dd6c9a257192cdc89cbe2
Manuel Moussallam <manuel.moussallam@gmail.com>
2013-01-31 10:34:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+pyCort
+======
+
+This project intend to transcribe existing tools for cochleogram and corticogram
+constructions  (especially the NLS Toolbox) in python language
+
+
+M. Moussallam - manuel.moussallam@espci.fr
+Jan 13
---
+++ src/sketch.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 30, 2013
+
+@author: manu
+'''
+
+class audSpectrum(object):
+    ''' an auditory spectrum '''
+
+
+def cochleogram(data, params):
+    ''' Build a cochleogram object from a set of parameters for
+    given data, in the form of an auditory spectrum ''' 
