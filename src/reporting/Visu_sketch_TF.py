ff00c7f2ab20a054fb2135df4b613f8db631ac42
Manuel Moussallam <manuel.moussallam@gmail.com>
2013-04-22 17:02:03 +0200

    changes

--- src/reporting/Visu_sketch_TF.py
+++ src/reporting/Visu_sketch_TF.py
@@ -29,20 +29,45 @@ audio_output_path = '/home/manu/workspace/audio-sketch/src/reporting/audio/'
 
 
 ########## plot spectrogram for glockenspiel ###########
-synth_sig = Signal(audio_test_file,normalize=True, mono=True)
-synth_sig.crop(0.1*synth_sig.fs, 3.5*synth_sig.fs)
+def expe_1():
+    synth_sig = Signal(audio_test_file,normalize=True, mono=True)
+    synth_sig.crop(0.1*synth_sig.fs, 3.5*synth_sig.fs)
+    
+    #synth_sig.resample(32000)
+    plt.figure(figsize=(10,5))
+    plt.subplot(211)
+    plt.plot(np.arange(.0,synth_sig.length)/float(synth_sig.fs), synth_sig.data)
+    plt.xticks([])
+    plt.ylim([-1,1])
+    plt.grid()
+    plt.subplot(212)
+    synth_sig.spectrogram(1024,64, order=0.25, log=False,
+                          cmap=cm.hot, cbar=False)
+    
+    plt.savefig(op.join(figure_output_path, 'glocs_spectro.pdf'))    
+    plt.show()
 
-#synth_sig.resample(32000)
-plt.figure(figsize=(10,5))
-plt.subplot(211)
-plt.plot(np.arange(.0,synth_sig.length)/float(synth_sig.fs), synth_sig.data)
-plt.xticks([])
-plt.ylim([-1,1])
-plt.grid()
-plt.subplot(212)
-synth_sig.spectrogram(1024,64, order=0.25, log=False,
-                      cmap=cm.hot, cbar=False)
+########### plot spectrogram for two different speakers ########"
+sig_1_path = '/sons/voxforge/main/Learn/cmu_us_slt_arctic/wav/arctic_a0372.wav' 
+sig_2_path = '/sons/voxforge/main/Learn/cmu_us_rms_arctic/wav/arctic_a0372.wav'
+i = 0
+for sig_path in [sig_1_path,sig_2_path]:
+    synth_sig = Signal(sig_path, normalize=True, mono=True)
+    #synth_sig.crop(0.1*synth_sig.fs, 3.5*synth_sig.fs)
+    #synth_sig.resample(32000)
+    plt.figure(figsize=(10,10))
+    plt.subplot(211)
+    plt.plot(np.arange(.0,synth_sig.length)/float(synth_sig.fs), synth_sig.data)
+    plt.xticks([])
+    plt.ylim([-1,1])
+    plt.grid()
+    plt.subplot(212)
+    synth_sig.spectrogram(1024,64, order=0.5, log=False,
+                          cmap=cm.hot, cbar=False)
+    
+    plt.savefig(op.join(figure_output_path, 'voice_%d_spectro.pdf'%i))   
+    synth_sig.write(op.join(audio_output_path, 'voice_%d_spectro.wav'%i))
+    i += 1
 
-plt.savefig(op.join(figure_output_path, 'glocs_spectro.pdf'))    
+ 
 plt.show()
-
